153b8bb39bf358e22bc688e1234eb8a614c1d01d
student <daniel.her@onja.org>
2021-06-05 08:29:36 +0300

    clean: removes build and ignored the typescript rule for useEffect

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -1,133 +1,134 @@
-import React, { createContext, useReducer, useEffect } from "react";
+import React, { createContext, useReducer, useEffect } from 'react'
 type State = {
-  countryData: any;
-  allData: any;
-  dispatch: React.Dispatch<any>;
-  mode: boolean;
-};
+  countryData: any
+  allData: any
+  dispatch: React.Dispatch<any>
+  mode: boolean
+}
 
 let initialState: State = {
   countryData: [],
   allData: [],
   dispatch: () => null,
   mode: false,
-};
+}
 
 type Country = [
   {
-    name: string;
-    topLevelDomain: string[];
-    alpha2Code: string;
-    alpha3Code: string;
-    callingCodes: string[];
-    capital: string;
-    altSpellings: string[];
-    region: string;
-    subregion: string;
-    population: number;
-    latlng: number[];
-    demonym: string;
-    area: number;
-    gini: number;
-    timezones: string[];
-    borders: string[];
-    nativeName: string;
-    numericCode: string;
+    name: string
+    topLevelDomain: string[]
+    alpha2Code: string
+    alpha3Code: string
+    callingCodes: string[]
+    capital: string
+    altSpellings: string[]
+    region: string
+    subregion: string
+    population: number
+    latlng: number[]
+    demonym: string
+    area: number
+    gini: number
+    timezones: string[]
+    borders: string[]
+    nativeName: string
+    numericCode: string
     currencies: [
       {
-        code: string;
-        name: string;
-        symbol: string;
+        code: string
+        name: string
+        symbol: string
       }
-    ];
+    ]
     languages: [
       {
-        iso639_1: string;
-        iso639_2: string;
-        name: string;
-        nativeName: string;
+        iso639_1: string
+        iso639_2: string
+        name: string
+        nativeName: string
       }
-    ];
+    ]
     translations: {
-      de: string;
-      es: string;
-      fr: string;
-      ja: string;
-      it: string;
-      br: string;
-      pt: string;
-      nl: string;
-      hr: string;
-      fa: string;
-    };
-    flag: string;
+      de: string
+      es: string
+      fr: string
+      ja: string
+      it: string
+      br: string
+      pt: string
+      nl: string
+      hr: string
+      fa: string
+    }
+    flag: string
     regionalBlocs: [
       {
-        acronym: string;
-        name: string;
-        otherAcronyms: string[];
-        otherNames: string[];
+        acronym: string
+        name: string
+        otherAcronyms: string[]
+        otherNames: string[]
       }
-    ];
-    cioc: string;
+    ]
+    cioc: string
   }
-];
+]
 
 type Action =
-  | { type: "LOADING"; payload: boolean }
-  | { type: "RESOLVED"; payload: Country[] }
-  | { type: "SEARCH_COUNTRY"; payload: Country[] }
-  | { type: "SEARCH_BY_REGION"; payload: Country[] }
-  | { type: "CHANGE_MODE"; payload: boolean }
-  | { type: "ERROR"; payload: string };
+  | { type: 'LOADING'; payload: boolean }
+  | { type: 'RESOLVED'; payload: Country[] }
+  | { type: 'SEARCH_COUNTRY'; payload: Country[] }
+  | { type: 'SEARCH_BY_REGION'; payload: Country[] }
+  | { type: 'CHANGE_MODE'; payload: boolean }
+  | { type: 'ERROR'; payload: string }
 
 function reducer(state: State = initialState, action: Action) {
   switch (action.type) {
-    case "RESOLVED":
+    case 'RESOLVED':
       return {
         ...state,
         isLoading: false,
         allData: action.payload,
         countryData: action.payload,
         error: null,
-      };
-    case "SEARCH_COUNTRY":
+      }
+    case 'SEARCH_COUNTRY':
       return {
         ...state,
         countryData: action.payload,
-      };
-    case "SEARCH_BY_REGION":
+      }
+    case 'SEARCH_BY_REGION':
       return {
         ...state,
         countryData: action.payload,
-      };
-    case "CHANGE_MODE":
+      }
+    case 'CHANGE_MODE':
       return {
         ...state,
         mode: action.payload,
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
-export const GlobalContext = createContext(initialState);
+export const GlobalContext = createContext(initialState)
 
 const GlobalProvider: React.FC = ({ children }) => {
-  const link = `https://restcountries.eu/rest/v2/all`;
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const link = `https://restcountries.eu/rest/v2/all`
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   async function fetchData() {
-    const countryData = await fetch(link);
-    const data = await countryData.json();
-    console.log(data);
+    const countryData = await fetch(link)
+    const data = await countryData.json()
+    console.log(data)
 
-    dispatch({ type: "RESOLVED", payload: data });
+    dispatch({ type: 'RESOLVED', payload: data })
   }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <GlobalContext.Provider
@@ -136,11 +137,10 @@ const GlobalProvider: React.FC = ({ children }) => {
         allData: state.allData,
         mode: state.mode,
         dispatch,
-      }}
-    >
+      }}>
       {children}
     </GlobalContext.Provider>
-  );
-};
+  )
+}
 
-export default GlobalProvider;
+export default GlobalProvider
